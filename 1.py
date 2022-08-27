9c6c35aa41fcd1e48536002c3a12e04f1f819820
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2022-08-27 13:15:22 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,42 @@
+# 1. Write a command to get the Python version you are using.
+# import sys
+# print(sys.version)
+
+# 2. Write a python script to print Hello Python on the screen directly in the command
+# line.
+# print("Hello Python")
+
+# 4. Write a python script to print Hello Python on the screen using visual studio code.
+# print("Hello Python")
+
+# 5. Write a python script to print Hello on the first line and Python on the second line
+# print(
+# '''Hello
+# Python''')
+
+# 6. Write a python script to print “MySirG” on the screen
+# print("\"MySirG\"")
+
+# 7. Write a python script to print “Teacher’s Day” on the screen
+# print("\"Teacher's Day\"")
+
+# 8. Write a python script to print the value of a variable. Variable contains your name as
+# data.
+# a = "Ankita"
+# print(a) 
+
+# 9. Write a python script to create variables to store your name, age, qualification, years
+# of experience and print it.
+# name="Ankita"
+# Age=18
+# Qualification="Diploma"
+# Experience=0
+# print(name,Age,Qualification,Experience,sep="\n")
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable
+# contains integer values
+# a=9
+# b=4
+# c=2
+# print(a,b,c,sep="\n")
+
